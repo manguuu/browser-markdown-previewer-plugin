22608b1e035ee2f6c547a305c616acc9a8dd09ea
Dong-Yun Park <ehddbs7458@gmail.com>
2021-11-20 13:58:49 +0900

    Initialize branch

--- iconTooltip_Popup/background.js
+++
@@ -1,8 +0,0 @@
-function getword(info, tab) {
-  chrome.extension.getBackgroundPage().console.log(info.selectionText);
-}
-chrome.contextMenus.create({
-  title: "Your Md Previewer: %s", 
-  contexts:["selection"],
-  onclick: getword
-});
