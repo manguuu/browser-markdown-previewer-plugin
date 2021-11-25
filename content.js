6e23c333efdcd00829a12eccf9bc0b171f185a91
mangu <alsrb4298@naver.com>
2021-11-25 13:56:02 +0900

    popup minsize and maxsize

--- content.js
+++ content.js
@@ -1,19 +1,14 @@
-/**tooltip */
-var tooltip = document.createElement("div");
-tooltip.setAttribute(
-    "id",
-    'markdown-box'
-);
-tooltip.setAttribute(
-    "class",
-    "markdown-view"
-);
-document.body.appendChild(tooltip);
-document.getElementById('markdown-box').style.visibility = 'hidden';
-tooltip.innerHTML = `<div class="markdown-header">Raw MD</div><p id="original"><div class="markdown-header">Preview</div></pr><p id="parsed">`;
 
 function makePopup() {
-  
+  var tooltip = document.createElement("div");
+  tooltip.setAttribute(
+    "id",
+    'markdown-box'
+    );
+
+  tooltip.innerHTML=`<div class="markdown-header">Raw MD</div><p id="original"><div class="markdown-header">Preview</div><p id="parsed">`;
+
+  document.body.appendChild(tooltip);
   document.getElementById('markdown-box').style.visibility = 'visible';
   document.getElementById('markdown-btn').style.visibility = 'hidden';
   const selection = window.getSelection();
@@ -23,9 +18,9 @@ function makePopup() {
   document.getElementById('markdown-box').style.top = scrollPosition + focus.top + 20 +"px";
   document.getElementById('markdown-box').style.left = focus.left + "px";
 
-  document.getElementById("original").innerHTML = selection.toString().replace(/(?:\r\n|\r|\n)/g, '<br />');
+  document.getElementById("original").innerHTML = "<div class='textbox'>" + selection.toString().replace(/(?:\r\n|\r|\n)/g, '<br>') + "</div>";
   const parsed = parseMd(selection.toString());
-  document.getElementById("parsed").innerHTML = parsed;
+  document.getElementById("parsed").innerHTML ="<div class='textbox'>" + parsed + "</div>";
 }
 /** tooltip icon */
 var btn = document.createElement("button");
@@ -33,43 +28,34 @@ btn.setAttribute(
   "id",
   "markdown-btn"
 );
-btn.setAttribute(
-    "class",
-    "markdown-view"
-);
 document.body.appendChild(btn);
 document.getElementById('markdown-btn').style.visibility = 'hidden';
 document.getElementById("markdown-btn").addEventListener("click",makePopup);
 btn.innerHTML=`<img src="https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/205_Markdown-1024.png" height="30" width="30">`
 
-/** documentclickevent*/
-
-document.addEventListener("mousedown", function (e) {
-    if (document.getElementById('markdown-btn').style.visibility == 'visible' && document.getElementById('markdown-box').style.visibility == 'hidden')
-        document.getElementById('markdown-btn').style.visibility = 'hidden';
-});
-
-
 document.addEventListener("mouseup", function (e) {
-    setTimeout($.proxy(function () {
-        var selectedText = document.getSelection().toString();
+  let selectedText = document.getSelection().toString();
 
-        if (document.getElementById('markdown-btn').style.visibility == 'hidden' && document.getElementById('markdown-box').style.visibility == 'hidden' && selectedText) {
-            var range = document.getSelection().getRangeAt(0);
-            var location = range.getBoundingClientRect();
-            var scrollPosition = $(window).scrollTop();
-            var Top = scrollPosition + location.top - 30 + "px";
-            var Left = location.left + "px";
+  if (selectedText.length ) {
+    let location = document.getSelection().getRangeAt(0).getBoundingClientRect();
+    let scrollPosition = $(window).scrollTop();
+    let Top = scrollPosition + location.top - 30 + "px";
+    let Left = location.left + "px";
 
-            btn.style.transform =
-                "translate3d(" + Left + "," + Top + "," + "0px)";
-            document.getElementById('markdown-btn').style.visibility = 'visible';
-        }
-        else if (!$(e.target).hasClass('markdown-view')) {
-            document.getElementById('markdown-btn').style.visibility = 'hidden';
-            if (document.getElementById('markdown-box') !== null) {
-                document.getElementById('markdown-box').style.visibility = 'hidden';
-            }
-        }
-    }, this), 0);
+//     var rect = e.target.getBoundingClientRect();
+//   var x = e.clientX - rect.left; //x position within the element.
+//   var y = e.clientY - rect.top;  //y position within the element.
+//       btn.style.top = y + 'px';
+//       btn.style.left = x + 'px';
+
+    btn.style.transform =
+      "translate3d(" + Left + "," + Top + "," + "0px)";
+      document.getElementById('markdown-btn').style.visibility = 'visible';
+  }
+  else if (!selectedText.length) {
+    document.getElementById('markdown-btn').style.visibility = 'hidden';
+    if (document.getElementById('markdown-box') !== null) {
+      document.getElementById('markdown-box').style.visibility = 'hidden';
+    }
+  }
 });
